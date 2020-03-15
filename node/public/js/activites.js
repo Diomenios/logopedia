3b1b8a10a2cce8ce3951ea2a02f5ea7a20f11555
diomenios <loui.arys@gmail.com>
2020-03-15 20:24:53 +0100

    activites.js et activites.html refactor ok

---
+++ node/public/js/activites.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const BUTTONS =[{url: "activites/activite-orthographe", id:"bouton_activités", name: "Orthographe"},
+              ];
+let vm;
+
+function init(){
+  
+  vm = new Vue({
+    el:"#activities-buttons",
+    data:{
+      buttons:BUTTONS,
+      hidden:"visible"
+    }
+  });
+}
