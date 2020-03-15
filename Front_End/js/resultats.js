d5629ba59b7f5c6037fc07f378db73e665736f70
Martinp-95 <m.perdaens@students.ephec.be>
2020-03-15 15:33:29 +0100

    Update JS/HTML page resultats

--- Front_End/js/resultats.js
+++ Front_End/js/resultats.js
@@ -25,6 +25,7 @@ var vuePatients = new Vue({
   el: '#divPatient',
   data: {
     message: '',
+    compteurPatientResultats:0,
     patients: []
   },
     methods: {
@@ -54,90 +55,85 @@ var vuePatients = new Vue({
            inputRecherche = document.getElementById('inputRecherche');
            inputRecherche.value = '';
            this.message = '';
-         }
+         },
+
+         resultats : function (){
+          let tabListePatients = [
+          {id:0, nom:"Pham", prenom:"Anh-Emile", age:24, activites:[12,14,10,9], activitesTentatives:[1,4,10,5]},
+          {id:1, nom:"Arys", prenom:"Louis", age:24, activites:[12,8,4,0], activitesTentatives:[1,4,10,5]},
+          {id:2, nom:"Arys", prenom:"Martin", age:24, activites:[12,18,0,9], activitesTentatives:[1,4,10,5]},
+          {id:3, nom:"Perdaens", prenom:"Martin", age:24, activites:[12,13,3,0], activitesTentatives:[1,4,10,5]},
+          {id:4, nom:"Dujardin", prenom:"Martin", age:24, activites:[12,9,7,0], activitesTentatives:[1,4,10,5]},
+          {id:5, nom:"Perdaens", prenom:"Céline", age:22, activites:[12,1,14,0], activitesTentatives:[1,4,10,5]},
+          {id:6, nom:"Perdaens", prenom:"Olivier", age:22, activites:[12,8,17,10], activitesTentatives:[1,4,10,5]}
+          ];
+           $("#ligneListe li").click(function () {
+            patient = this.textContent;
+            patientModif = patient.replace(/\s/g,"")
+            for(indicepatient of tabListePatients){
+              if((`${indicepatient.prenom}${indicepatient.nom}`) == patientModif){
+              this.compteurPatientResultats = indicepatient.id
+              break
+              }
+            }
+
+          $(document).ready(function(){
+            $('#ligneListe').click(function(){
+                $('#resultatsGraph').show();
+                $('#recherchePatients').hide();
+                $('#patients').hide();
+            });
+        
+            $('#boutonRetour').click(function(){
+              $('#resultatsGraph').hide();
+              $('#recherchePatients').show();
+              $('#patients').show();
+          });
+          });
+
+        let resultats = '<p>';
+        resultats += `<h3>Patient: <span id="infoPatient">${tabListePatients[this.compteurPatientResultats].prenom} ${tabListePatients[this.compteurPatientResultats].nom}
+        </span></h3>
+        <table id="tableResultats">
+        <thead>
+        <tr>
+        <th> Activité 1 </th>
+        <th> Activité 2 </th>
+        <th> Activité 3 </th>
+        <th> Activité 4 </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td>${tabListePatients[this.compteurPatientResultats].activites[0]}</td>
+        <td>${tabListePatients[this.compteurPatientResultats].activites[1]}</td>
+        <td>${tabListePatients[this.compteurPatientResultats].activites[2]}</td>
+        <td>${tabListePatients[this.compteurPatientResultats].activites[3]}</td>
+        </tr>
+        </tbody> 
+        </table>`;
+
+      resultats += '</p>'
+      +'<h3>Nombre de tentatives par activités:</h3>'
+      +'<canvas id="graphique1"></canvas>'
+      //+'<h3>Points par activités:</h3>'
+      //+'<canvas id="graphique2"></canvas>';
+
+      document.getElementById("resultatTexteGraph").innerHTML = resultats;
+    })
+       
+    }
       }
+      
     })
 
   for( listePatient of tabListePatients){
-    vuePatients.patients.push({ text:`${listePatient.prenom} ${listePatient .nom}`})
+    vuePatients.patients.push({ text:`${listePatient.prenom} ${listePatient.nom}`})
   }
 
 
 
 /*
-
-
-//Fonction qui permet de faire le switch entre la liste et les différentes forment de résiltats
-function goResultats(){
-  //TODO
-  let tabListePatients = [ // J'ai mis le tableau la de manière temporaire car quand il ne se trouve pas dans la fonction rien ne s'affiche dans les graph
-    {id:0, nom:"Pham", prenom:"Anh-Emile", age:24, activites:[12,14,10,9], activitesTentatives:[1,4,10,5]},
-    {id:1, nom:"Arys", prenom:"Louis", age:24, activites:[12,8,4,0], activitesTentatives:[1,4,10,5]},
-    {id:2, nom:"Arys", prenom:"Martin", age:24, activites:[12,18,0,9], activitesTentatives:[1,4,10,5]},
-    {id:3, nom:"Perdaens", prenom:"Martin", age:24, activites:[12,13,3,0], activitesTentatives:[1,4,10,5]},
-    {id:4, nom:"Dujardin", prenom:"Martin", age:24, activites:[12,9,7,0], activitesTentatives:[1,4,10,5]},
-    {id:5, nom:"Perdaens", prenom:"Céline", age:22, activites:[12,1,14,0], activitesTentatives:[1,4,10,5]},
-    {id:6, nom:"Perdaens", prenom:"Olivier", age:22, activites:[12,8,17,10], activitesTentatives:[1,4,10,5]}
-    ];
-
-    //TODO problème au niv de récupére l'id ???????????????????????????????????
-    let compteurPatientResultats = 0;
-    $("#ligneListe li").click(function () {
-      patient = this.textContent;
-      console.log(patient)
-      console.log(tabListePatients[0].nom+'prout')
-      console.log(patient.split(' ')[1].replace("\n","")+'prout')
-      console.log(tabListePatients[0].nom === patient.split(' ')[1])
-      for(indicepatient of tabListePatients){
-        if(indicepatient.nom == patient.split(' ')[1]){
-          compteurPatientResultats = indicepatient.id
-        }
-      }
-
-  $(document).ready(function(){
-    $('#ligneListe').click(function(){
-        $('#resultatsGraph').show();
-        $('#divPatient').hide();
-    });
-
-    $('#boutonRetour').click(function(){
-      $('#resultatsGraph').hide();
-      $('#divPatient').show();
-  });
-  });
-  //-------------------------------------------------------
-  //Partie résultats texte
-
-  let resultats = '<p>';
-      resultats += `<h3>Patient: <span id="infoPatient">${tabListePatients[compteurPatientResultats].prenom} ${tabListePatients[compteurPatientResultats].nom}</span></h3>
-      <table id="tableResultats">
-      <thead>
-      <tr>
-      <th> Activité 1 </th>
-      <th> Activité 2 </th>
-      <th> Activité 3 </th>
-      <th> Activité 4 </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-      <td>${tabListePatients[compteurPatientResultats].activites[0]}</td>
-      <td>${tabListePatients[compteurPatientResultats].activites[1]}</td>
-      <td>${tabListePatients[compteurPatientResultats].activites[2]}</td>
-      <td>${tabListePatients[compteurPatientResultats].activites[3]}</td>
-      </tr>
-      </tbody> 
-      </table>`;
-  //}
-
-  resultats += '</p>'
-  +'<h3>Nombre de tentatives par activités:</h3>'
-  +'<canvas id="graphique1"></canvas>'
-  +'<h3>Points par activités:</h3>'
-  +'<canvas id="graphique2"></canvas>';
-
-  document.getElementById("resultatTexteGraph").innerHTML = resultats;
-
   //-------------------------------------------------------
   //Partie résultats Graphiques
   //partie pour le graph en formage
@@ -180,5 +176,4 @@ new Chart(document.getElementById("graphique2"), {
     legend: { display: false }
   }
   }); // fin du graph bat ici
-});
-}*/
+*/
