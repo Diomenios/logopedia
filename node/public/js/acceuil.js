eaad4453ce5e2056b828219233d32ad100f3184b
diomenios <loui.arys@gmail.com>
2020-03-25 00:28:23 +0100

    cleanup du code acceuil.js et acceuil.html + set-up présentation

--- node/public/js/acceuil.js
+++ node/public/js/acceuil.js
@@ -2,10 +2,6 @@
 
 const ACTIVITIES_LIST = ["Orthographe", "Vocabulair","Drag et Drop"];
 
-let vm = new Vue({
-	el: '#'
-});
-
 function connexion(){
 	let connect = '<div class="row">'
 					+'<div class="col-sm-9 col-md-7 col-lg-5 mx-auto"><div class="card card-signin my-5"><div class="card-body">'
